d650da470f1cb85e4970ca24bb85628a3b507671
Lavanya Cheemakurthy S <chslavanya@yahoo.com>
2021-01-05 23:06:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
+import {Button} from './components/common/styledComponents'
+import NavBar from './components/navbar/navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar></NavBar>
+          <Button>Click</Button>
+        
       </header>
     </div>
   );
---
+++ src/components/data/data.js
@@ -0,0 +1,15 @@
+export const navs =[
+    {
+        navName="Home"
+    }
+]
+export const intro = {
+    name:"Micky",
+    profession:"",
+    notes:"I am am am,.....",
+    personalDetails:{
+        email:"",
+        dob:"",
+        phone:""
+    }
+}
