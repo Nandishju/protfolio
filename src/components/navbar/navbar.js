90e9561026833f61cb611063b6620e00bad60d79
LavanyaChs <chslavanya@yahoo.com>
2021-06-13 23:53:58 +0530

    Renamed contact us to contact

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -42,7 +42,7 @@ export default class NavBar extends React.Component {
               <li>skills</li>
             </NavLink>
             <NavLink activeClassName="active" to="/contact">
-              <li>Contact Us</li>
+              <li>Contact </li>
             </NavLink>
           </ul>
           <div id="hero-wave" class="css-1qmcrmh ehoxvtz0"><svg viewBox="0 0 1695 57" preserveAspectRatio="none"><path d="M0 23c135.4 19 289.6 28.5 462.5 28.5C721.9 51.5 936.7 1 1212.2 1 1395.8.9 1556.7 8.3 1695 23v34H0V23z" fill="rgba(255,255,255,1)" fill-rule="evenodd"></path></svg></div>
